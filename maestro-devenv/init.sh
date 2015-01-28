bf5d2c83a3eb8bb6b3e3f162247a7700ff3bc9ea
Luke Williams <luke.williams@cba.com.au>
2015-01-28 20:46:40 +1100

    Add support for installing certificates during initialisation.

--- maestro-devenv/init.sh
+++ maestro-devenv/init.sh
@@ -8,7 +8,8 @@
 #
 #  Before we can run puppet, we need to:
 #    1. Test and configure the proxy, if required.
-#    3. Add the puppet repositories to apt.
+#    2. Install any required certificates.
+#    3. Download and install the puppet repository metapackage.
 #    4. Update the apt package list.
 #    5. Install puppet.
 # 
@@ -20,10 +21,12 @@ PROXY="$PROXY_HOST:$PROXY_PORT"
 PROXY_HTTP="http://$PROXY"
 PROXY_HTTPS="https://$PROXY"
 PROXY_APT="/etc/apt/apt.conf.d/120proxy"
-PROXY_PROFILE="/etc/profile.d/10proxy"
+PROXY_PROFILE="/etc/profile.d/10proxy.sh"
 
 PUPPET_PKG_URL="https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb"
 PUPPET_PKG_NAME="puppetlabs-release-wheezy.deb"
+
+ECHO="echo [maestro-init]"
 
 #
 # Proxy
@@ -31,98 +34,131 @@ PUPPET_PKG_NAME="puppetlabs-release-wheezy.deb"
 
 if [ $PROXY_ENABLE == "yes" ]
 then
-    echo Configuring proxy:
+    $ECHO Configuring proxy:
     
-    echo -n "    Testing connectivity... "
+    $ECHO "  Testing connectivity..."
     exec 6<>/dev/tcp/$PROXY_HOST/$PROXY_PORT
     E=$?
     if [ $E -eq 0 ]
     then
-        echo connection established to $PROXY.
+        $ECHO "  ...connection established to $PROXY."
         # Always remember to clean up :)
         exec 6>&-
         exec 6<&-
     else
-        echo FAILED to establish a connection to $PROXY \($E\).
-        echo Unable to connect to proxy server. Possible causes:
-        echo   - Forgot to add 'Gateway yes' to /usr/local/etc/cntlm.conf
-        echo   - Forgot to restart cntlm after modifying cntlm.conf
-        echo   - Changed network settings in Vagrantfile but not bootstrap.sh
+        $ECHO "  ...FAILED to establish a connection to $PROXY \($E\)."
+        $ECHO Unable to connect to proxy server. Possible causes:
+        $ECHO   - Forgot to add 'Gateway yes' to /usr/local/etc/cntlm.conf
+        $ECHO   - Forgot to restart cntlm after modifying cntlm.conf
+        $ECHO   - Changed network settings in Vagrantfile but not bootstrap.sh
         exit $E
     fi
     
-    echo -n "    Configuring system profile... "
+    $ECHO "  Configuring system profile..."
     if [ ! -f $PROXY_PROFILE ]
     then
         touch $PROXY_PROFILE
-        echo export http_proxy=$PROXY_HTTP >> $PROXY_PROFILE
-        echo export https_proxy=$PROXY_HTTPS >> $PROXY_PROFILE
-        . $PROXY_PROFILE # We'll need this to wget the puppet repo deb
-        echo created $PROXY_PROFILE.
+        $ECHO export http_proxy=$PROXY_HTTP >> $PROXY_PROFILE
+        $ECHO export https_proxy=$PROXY_HTTPS >> $PROXY_PROFILE
+        $ECHO "  ...created $PROXY_PROFILE."
     else
-        echo file $PROXY_PROFILE exists, skipping.
+        $ECHO "  ...file $PROXY_PROFILE exists, skipping."
     fi
     
-    echo -n "    Configuring apt... "
+    $ECHO "    Configuring apt..."
     if [ ! -f $PROXY_APT ]
     then
         touch $PROXY_APT
-        echo Acquire::http::Proxy \"$PROXY_HTTP\"; >> $PROXY_APT
-        echo Acquire::https::Proxy \"$PROXY_HTTPS\"; >> $PROXY_APT
-        echo created $PROXY_APT.
+        $ECHO Acquire::http::Proxy \"$PROXY_HTTP\"\; >> $PROXY_APT
+        $ECHO Acquire::https::Proxy \"$PROXY_HTTPS\"\; >> $PROXY_APT
+        $ECHO "  ...created $PROXY_APT."
     else
-        echo file $PROXY_APT exists, skipping.
+        $ECHO "  ...file $PROXY_APT exists, skipping."
+    fi
+    
+    . $PROXY_PROFILE # We'll need this for wget in the next step
+fi
+
+#
+# Certificates
+#
+
+
+$ECHO Installing certificates...
+
+N_CERTS=$(ls /vagrant/certs/ | grep -v README | wc -l)
+if [ $N_CERTS == "0" ]
+then
+    $ECHO "...no certificates to install"
+else
+    cp -r /vagrant/certs /usr/local/share/ca-certificates/maestro
+    for i in /usr/local/share/ca-certificates/maestro/*.pem
+    do
+        if [ -r $i ]
+        then
+            mv $i $i.crt
+        fi
+    done
+    
+    update-ca-certificates > /dev/null
+    E=$?
+    if [ $E -eq 0 ]
+    then
+        $ECHO ...installed $N_CERTS certificate\(s\).
+    else
+        $ECHO FAILED \($E\).
+        exit $E
     fi
 fi
+
 
 #
 # Puppet
 #
 
-echo Configuring puppet:
+$ECHO Configuring puppet:
 
-echo -n "    Downloading repository metapackage... "
+$ECHO "  Downloading repository metapackage..."
 wget --quiet -P /tmp $PUPPET_PKG_URL
 E=$?
 if [ $E -eq 0 ]
 then
-    echo saved to /tmp/$PUPPET_PKG_NAME.
+    $ECHO "  ...saved to /tmp/$PUPPET_PKG_NAME."
 else
-    echo FAILED \($E\).
+    $ECHO "  ...FAILED \($E\)."
     exit $E
 fi
 
-echo -n "    Installing repository metapackage... "
-dpkg -i /tmp/$PUPPET_PKG_NAME
+$ECHO "  Installing repository metapackage..."
+dpkg -i /tmp/$PUPPET_PKG_NAME > /dev/null
 E=$?
 if [ $E -eq 0 ]
 then
-    echo installed.
+    $ECHO "  ...installed."
 else
-    echo FAILED \($E\).
+    $ECHO "  ...FAILED \($E\)."
     exit $E
 fi
 
-echo -n "    Updating package lists... "
-apt-get update
+$ECHO "  Updating package lists..."
+apt-get -qq update
 E=$?
 if [ $E -eq 0 ]
 then
-    echo updated.
+    $ECHO "  ...updated."
 else
-    echo FAILED \($E\).
+    $ECHO "  ...FAILED \($E\)."
     exit $E
 fi
 
-echo -n "    Installing puppet... "
+$ECHO "  Installing puppet... "
 apt-get -y -qq install puppet > /dev/null
 E=$?
 if [ $E -eq 0 ]
 then
-    echo installed.
+    $ECHO "  ...installed."
 else
-    echo FAILED \($E\).
+    $ECHO "  ...FAILED \($E\)."
     exit $E
 fi
-
 
