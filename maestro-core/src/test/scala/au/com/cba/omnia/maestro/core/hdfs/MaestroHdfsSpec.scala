57b5d0444b5d2d765234b9af2e48dd44a4e25221
Todd Owen <todd.owen@cba.com.au>
2015-04-15 16:45:21 +1000

    Adds MaestroHdfs and deprecates Guard
    This fixes #285, as the new methods return Hdfs actions.

--- maestro-core/src/test/scala/au/com/cba/omnia/maestro/core/hdfs/MaestroHdfsSpec.scala
+++ maestro-core/src/test/scala/au/com/cba/omnia/maestro/core/hdfs/MaestroHdfsSpec.scala
@@ -1,13 +1,22 @@
 package au.com.cba.omnia.maestro.core.hdfs
 
+import org.apache.hadoop.conf.Configuration
+
+import org.specs2.execute.AsResult
 import org.specs2.matcher.FileMatchers
+import org.specs2.specification.AroundExample
 
 import au.com.cba.omnia.thermometer.core.{ThermometerSpec, Thermometer}, Thermometer._
 
-class GuardSpec extends ThermometerSpec with FileMatchers { def is = s2"""
+import au.com.cba.omnia.permafrost.test.HdfsMatchers
 
-HDFS Guard properties
-=====================
+class MaestroHdfsSpec extends ThermometerSpec
+  with AroundExample
+  with FileMatchers
+  with HdfsMatchers { def is = s2"""
+
+MaestroHdfs properties
+======================
 
   expandPaths:
     matches globbed dirs               $expandPaths_matchesGlobbedDirs
@@ -25,9 +34,15 @@ HDFS Guard properties
     creates _PROCESSED                 $createFlagFile_createsPROCESSED
 """
 
+  // Used by HdfsMatchers
+  val conf = new Configuration
+
+  // Every test executes with a copy of src/test/resources/hdfs-guard/
+  def around[T: AsResult](t: => T) = withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) { AsResult(t) }
+
   def expandPaths_matchesGlobbedDirs = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      Guard.expandPaths(s"$dir/user/a*") must containTheSameElementsAs(List(
+    MaestroHdfs.expandPaths(s"$dir/user/a*") must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/a",
         s"file:$dir/user/a1"
         // excludes various other directories that don't match the glob
@@ -36,8 +51,8 @@ HDFS Guard properties
   }
 
   def expandPaths_skipsFiles = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      Guard.expandPaths(s"$dir/user/b*") must containTheSameElementsAs(List(
+    MaestroHdfs.expandPaths(s"$dir/user/b*") must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/b1"
         // excludes "b2" because it's not a directory
       ))
@@ -45,8 +60,8 @@ HDFS Guard properties
   }
 
   def expandPaths_skipsProcessed = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      Guard.expandPaths(s"$dir/user/c*") must containTheSameElementsAs(List(
+    MaestroHdfs.expandPaths(s"$dir/user/c*") must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/c",
         s"file:$dir/user/c_transferred"
         // excludes "c_processed"
@@ -55,8 +70,8 @@ HDFS Guard properties
   }
 
   def expandTransferredPaths_skipsUningested = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      Guard.expandTransferredPaths(s"$dir/user/c*") must containTheSameElementsAs(List(
+    MaestroHdfs.expandTransferredPaths(s"$dir/user/c*") must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/c_transferred"
         // excludes "c"
         // excludes "c_processed"
@@ -65,9 +80,9 @@ HDFS Guard properties
   }
 
   def listNonEmptyFiles_listsNonEmptyFiles = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      val paths = List(s"$dir/user/a", s"$dir/user/c")
-      Guard.listNonEmptyFiles(paths) must containTheSameElementsAs(List(
+    val paths = List(s"$dir/user/a", s"$dir/user/c")
+    MaestroHdfs.listNonEmptyFiles(paths) must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/c/c.dat"
         // excludes "a/a.dat" because it is zero-length
       ))
@@ -75,9 +90,9 @@ HDFS Guard properties
   }
 
   def listNonEmptyFiles_skipsSubdirectories = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      val paths = List(s"$dir/user")
-      Guard.listNonEmptyFiles(paths) must containTheSameElementsAs(List(
+    val paths = List(s"$dir/user")
+    MaestroHdfs.listNonEmptyFiles(paths) must beValueLike {
+      _ must containTheSameElementsAs(List(
         s"file:$dir/user/b2"
         // excludes all subdirectories
       ))
@@ -85,11 +100,9 @@ HDFS Guard properties
   }
 
   def createFlagFile_createsPROCESSED = {
-    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      val paths = List(s"$dir/user/a1", s"$dir/user/b1")
-      Guard.createFlagFile(paths)
-      s"$dir/user/a1/_PROCESSED" must beAFilePath
-      s"$dir/user/b1/_PROCESSED" must beAFilePath
-    }
+    val paths = List(s"$dir/user/a1", s"$dir/user/b1")
+    MaestroHdfs.createFlagFile(paths) must beValue(())
+    s"$dir/user/a1/_PROCESSED" must beAFilePath
+    s"$dir/user/b1/_PROCESSED" must beAFilePath
   }
 }
